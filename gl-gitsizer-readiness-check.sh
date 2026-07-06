c9fd36d8269b9a3c60fa19c304f0e150184f816c
pauljaison <79197694+pauljaison@users.noreply.github.com>
2026-07-06 16:42:03 +0530

    Update gl-gitsizer-readiness-check.sh

--- gl-gitsizer-readiness-check.sh
+++ gl-gitsizer-readiness-check.sh
@@ -558,11 +558,11 @@ run_checks() {
 
       {
         echo "------------------------------------------------------------"
-        echo "Repository                           : $repo_name"
-        echo "Path                                 : $project_path"
-        echo "Repo Size                            : ${repo_size_mb} MB"
-        echo "Largest File Size                    : ${largest_blob_mb} MB"
-        echo "Files > ${THRESHOLD_MB} MB found     : $large_file_count"
+        echo "Repository                        : $repo_name"
+        echo "Path                              : $project_path"
+        echo "Repo Size                         : ${repo_size_mb} MB"
+        echo "Largest File Size                 : ${largest_blob_mb} MB"
+        echo "Files > ${THRESHOLD_MB} MB found  : $large_file_count"
         echo "------------------------------------------------------------"
 
         while IFS=$'\t' read -r blob_sha blob_size_mb file_path; do
